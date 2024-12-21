1388563008cfdb68d49726a7fd8a26a98be75285
CDave2 <banlutachristiandave@gmail.com>
2024-12-21 21:08:45 +0800

    import line main dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/screens/dasboard_screen.dart';
 import 'package:flutter_demo/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Import the generated file
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Provide the options
+  );
   runApp(MyApp());
 }
 
@@ -13,7 +16,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-   @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -29,4 +32,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
